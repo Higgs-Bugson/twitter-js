e33977da88ccf4e59bf5cd9baf45a7fb1ba40d65
Tania Santamaria <tsantamaria.r@gmail.com>
2018-03-06 12:02:39 -0500

    first template

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
  const express= require("express");
  const app= express();
 const volleyball=require("volleyball");
+const nunjucks = require("nunjucks");
 
 
 
@@ -16,6 +17,16 @@ const volleyball=require("volleyball");
 //      next();
 //  })
 
+const locals = {
+    title: 'My Tweeter', 
+    head: 'welcome',
+    people: [
+        {name: 'Gandalf'}, 
+        {name: 'Frodo'},
+        {name: 'Hermione'}
+    ]
+}
+
 app.use(volleyball);
  app.get('/special/',(req,res)=>{
      res.send("we are special")
@@ -27,6 +38,19 @@ app.get('/', (req,res)=>{
 app.get('/news', (req,res)=>{
     res.send("TODAY'S HEADLINES")
 });
+
+app.get('/twitter', (req,res)=>{
+    res.render('index', {title: 'Twitter', head: locals.head, people: locals.people});
+});
+
+
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function(err, output){
+    console.log(output);
+})
 
 app.listen(3000,()=>{
     console.log("listening on port 3000")
