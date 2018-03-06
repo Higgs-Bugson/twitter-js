06d4bde9003039f034514a66aad7c85a2c6991f6
Tania Santamaria <tsantamaria.r@gmail.com>
2018-03-06 11:22:31 -0500

    Step 3 completed

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const express= require("express");
-const app= express();
+ const express= require("express");
+ const app= express();
+const volleyball=require("volleyball");
+
+
+
+// app.use(function(req,res,next){
+//    console.log(`${req.method} /  ${req.path}`)
+   
+//     console.log(`${req.method} /  ${res.statusCode}`)
+//     next();
+// })
+// app.use('/special/',function(req,res,next){
+//     console.log("You have reached a special area")
+   
+//      next();
+//  })
+
+app.use(volleyball);
+ app.get('/special/',(req,res)=>{
+     res.send("we are special")
+ });
+app.get('/', (req,res)=>{
+    res.send("Welcome")
+});
+
+app.get('/news', (req,res)=>{
+    res.send("TODAY'S HEADLINES")
+});
 
 app.listen(3000,()=>{
     console.log("listening on port 3000")
 });
-
-
-app.get('/', (req,res)=>{
-    res.send("Welcome")
-});
+//console.log(app.request.path);
